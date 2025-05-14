b284949a3546e95ba011d1012f7e840a484a4e4a
surya <surya.prakash1339@gmail.com>
2025-05-14 23:25:30 +0530

    rg_variable

---
+++ variable/main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "surya" {
+  for_each = var.resource_group_name 
+    name     = each.value.rg_name
+    location = each.value.location
+}
+
+variable "resource_group_name" {
+  type = any
+}
---
+++ variable/terraform.tfvars
@@ -0,0 +1,10 @@
+resource_group_name = {
+    "rg1" = {
+        rg_name  = "rg11"
+        location = "East US"
+        }
+    "rg2" = {
+        rg_name  = "rg12"
+        location = "East US"
+    }
+}
